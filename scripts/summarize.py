29becf190072762cf8ff26605710602b98fd1549
Pete Bachant <petebachant@gmail.com>
2025-12-17 15:25:52 -0800

    Rework summarize script

--- scripts/summarize.py
+++ scripts/summarize.py
@@ -1,12 +1,14 @@
 """Extract a summary from an nsys report file."""
 
+import glob
+import os
 import sqlite3
 
 import polars as pl
 
 
 def get_summary(fpath_in) -> dict:
-    """Extract summary from nsys report file."""
+    """Extract summary from nsys SQLite report file."""
     conn = sqlite3.connect(fpath_in)
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM ANALYSIS_DETAILS")
@@ -18,13 +20,18 @@ def get_summary(fpath_in) -> dict:
     return summary
 
 
-configs = ["baseline", "mod", "baseline-const", "baseline-none", "mod-const"]
+sqlite_fpaths = sorted(glob.glob("results/nsys/*.sqlite"))
 results = []
 
-for config in configs:
-    fpath_in = f"results/nsys/{config}.sqlite"
-    summary = {"case": config} | get_summary(fpath_in)
-    results.append(summary)
+for fpath in sqlite_fpaths:
+    config = os.path.basename(fpath).removesuffix(".sqlite")
+    print(f"Processing {config} from {fpath}")
+    try:
+        summary = {"case": config} | get_summary(fpath)
+        results.append(summary)
+    except Exception as e:
+        print(f"Failed to process {config}: {e}")
 
 df = pl.DataFrame(results)
+print(df)
 df.write_csv("results/summary.csv")
